be31fe090b38642478ea27d6abbb60321572d8b4
k-kams <jh97.dev@gmail.com>
2023-02-24 13:35:01 +0900

    added tests that assert logs

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from unittest import TestCase
 
 import pytest
@@ -7,12 +8,14 @@ from django.urls import reverse
 
 from companies.models import Company
 
+logger = logging.getLogger("CORONA_LOGS")
+
 
 @pytest.mark.django_db
 class BasicCompanyAPITestCase(TestCase):
     def setUp(self) -> None:
         self.client = Client()
-        self.companies_url = reverse('companies-list')
+        self.companies_url = reverse("companies-list")
 
     def tearDown(self) -> None:  # 테스트가 실행될 때마다 실행.
         pass
@@ -25,15 +28,15 @@ class TestGetCompanies(BasicCompanyAPITestCase):
         self.assertEqual(json.loads(response.content), [])
 
     def test_one_company_exists_should_succeeed(self) -> None:
-        test_company = Company.objects.create(name='Amazon')
+        test_company = Company.objects.create(name="Amazon")
         response = self.client.get(self.companies_url)
         # print(response.content)  # pytest -v -s
         response_content = json.loads(response.content)[0]
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response_content.get('name'), test_company.name)
-        self.assertEqual(response_content.get('status'), 'Hiring')
-        self.assertEqual(response_content.get('application_link'), '')
-        self.assertEqual(response_content.get('notes'), '')
+        self.assertEqual(response_content.get("name"), test_company.name)
+        self.assertEqual(response_content.get("status"), "Hiring")
+        self.assertEqual(response_content.get("application_link"), "")
+        self.assertEqual(response_content.get("notes"), "")
 
         test_company.delete()
 
@@ -43,38 +46,46 @@ class TestPostCompanies(BasicCompanyAPITestCase):
         response = self.client.post(path=self.companies_url)
         self.assertEqual(response.status_code, 400)
         self.assertEqual(
-            json.loads(response.content), {'name': ['This field is required.']}
+            json.loads(response.content), {"name": ["This field is required."]}
         )
 
     def test_create_existing_company_should_fail(self) -> None:
-        Company.objects.create(name='apple')
-        response = self.client.post(path=self.companies_url, data={'name': 'apple'})
+        Company.objects.create(name="apple")
+        response = self.client.post(path=self.companies_url, data={"name": "apple"})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(
             json.loads(response.content),
-            {'name': ['company with this name already exists.']}
+            {"name": ["company with this name already exists."]},
         )
 
     def test_create_company_with_only_name_all_fields_should_be_default(self) -> None:
-        response = self.client.post(path=self.companies_url, data={'name': 'test company name'})
+        response = self.client.post(
+            path=self.companies_url, data={"name": "test company name"}
+        )
         self.assertEqual(response.status_code, 201)
         response_content = json.loads(response.content)
-        self.assertEqual(response_content.get('name'), 'test company name')
-        self.assertEqual(response_content.get('status'), 'Hiring')
-        self.assertEqual(response_content.get('application_link'), '')
-        self.assertEqual(response_content.get('notes'), '')
+        self.assertEqual(response_content.get("name"), "test company name")
+        self.assertEqual(response_content.get("status"), "Hiring")
+        self.assertEqual(response_content.get("application_link"), "")
+        self.assertEqual(response_content.get("notes"), "")
 
     def test_create_company_with_layoffs_status_should_succeed(self) -> None:
-        response = self.client.post(path=self.companies_url, data={'name': 'test company name', 'status': 'Layoffs'})
+        response = self.client.post(
+            path=self.companies_url,
+            data={"name": "test company name", "status": "Layoffs"},
+        )
         self.assertEqual(response.status_code, 201)
         response_content = json.loads(response.content)
-        self.assertEqual(response_content.get('status'), 'Layoffs')
+        self.assertEqual(response_content.get("status"), "Layoffs")
 
     def test_create_company_with_wrong_status_should_fail(self) -> None:
-        response = self.client.post(path=self.companies_url, data={'name': 'test company name', 'status': 'WrongStatus'})
+        response = self.client.post(
+            path=self.companies_url,
+            data={"name": "test company name", "status": "WrongStatus"},
+        )
         self.assertEqual(response.status_code, 400)
-        self.assertIn('WrongStatus', str(response.content))
-        self.assertIn('is not a valid choice', str(response.content))
+        self.assertIn("WrongStatus", str(response.content))
+        self.assertIn("is not a valid choice", str(response.content))
 
     @pytest.mark.xfail
     def test_should_be_ok_if_false(self) -> None:
@@ -83,3 +94,35 @@ class TestPostCompanies(BasicCompanyAPITestCase):
     @pytest.mark.skip
     def test_should_be_skipped(self) -> None:
         self.assertEqual(1, 2)
+
+    def raise_covid19_exception(self) -> None:
+        raise ValueError("CoronaVirus Exception")
+
+    def test_raise_covid19_exception_should_pass(self) -> None:
+        """
+        pytest -k test_raise_covid19_exception_should_pass
+        """
+        with pytest.raises(ValueError) as e:
+            self.raise_covid19_exception()
+        assert "CoronaVirus Exception" == str(e.value)
+
+
+def function_that_logs_something() -> None:
+    try:
+        raise ValueError("CoronaVirus Exception")
+    except ValueError as e:
+        logger.warning(f"I am logging {str(e)}")
+
+
+def test_logged_warning_level(caplog) -> None:
+    """
+    pytest {path} -k 'logged'
+    """
+    function_that_logs_something()
+    assert "I am logging CoronaVirus Exception" in caplog.text
+
+
+def test_logged_info_level(caplog) -> None:
+    with caplog.at_level(logging.INFO):
+        logger.info("I am logging info level")
+        assert "I am logging info level" in caplog.text
