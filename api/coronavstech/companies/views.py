b53de59088846c6648a7d2f4c86df2787de7fe05
k-kams <jh97.dev@gmail.com>
2023-03-06 21:48:23 +0900

    add email mechanism

--- api/coronavstech/companies/views.py
+++ api/coronavstech/companies/views.py
@@ -1,4 +1,8 @@
+from django.core.mail import send_mail
+from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from companies.models import Company
@@ -7,5 +11,21 @@ from companies.serializers import CompanySerializer
 
 class CompanyViewSet(ModelViewSet):
     serializer_class = CompanySerializer
-    queryset = Company.objects.all().order_by("-last_update")
+    queryset = Company.objects.all().order_by('-last_update')
     pagination_class = PageNumberPagination
+
+
+@api_view(http_method_names=['POST'])
+def send_company_email(request: Request) -> Response:
+    '''
+    sends email with request payload
+    sender: isrealtechlayoffs@gmail.com
+    receiver: iserealtechlayoffs@gmail.com
+    '''
+    send_mail(
+        subject=request.data.get('subject'),
+        message=request.data.get('message'),
+        from_email='do8972@gmail.com',
+        recipient_list=['do8972@gmail.com']
+    )
+    return Response({'status': 'success', 'info': 'email sent successfully'}, status=200)
