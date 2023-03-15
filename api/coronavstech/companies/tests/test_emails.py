9e2b8bbdf2309787a7a56308564772f0320933b3
k-kams <jh97.dev@gmail.com>
2023-03-15 18:36:21 +0900

    refectoring from unittest to pytest

--- api/coronavstech/companies/tests/test_emails.py
+++ api/coronavstech/companies/tests/test_emails.py
@@ -1,3 +1,6 @@
+import json
+from unittest.mock import patch
+
 from django.core import mail
 
 # https://pytest-django.readthedocs.io/en/latest/helpers.html#mailoutbox
@@ -22,3 +25,27 @@ def test_send_email_should_succeed(mailoutbox, settings) -> None:
 
     # Verify that the subject of the first message is correct.
     assert mailoutbox[0].subject == 'Test Subject here'
+
+
+def test_send_email_without_arguments_should_send_empty_email(client) -> None:
+    with patch(
+        'companies.views.send_mail'
+    ) as mocked_send_mail_function:
+        response = client.post(path='/send-email')
+        response_content = json.loads(response.content)
+        assert response.status_code == 200
+        assert response_content["status"] == 'success'
+        assert response_content["info"] == 'email sent successfully'
+
+        mocked_send_mail_function.assert_called_with(
+            subject=None,
+            message=None,
+            from_email='do8972@gmail.com',
+            recipient_list=['do8972@gmail.com'],
+        )
+
+
+def test_send_email_with_get_verb_should_fail(client) -> None:
+    response = client.get(path='/send-email')
+    assert response.status_code == 405
+    assert json.loads(response.content) == {'detail': 'Method "GET" not allowed.'}
